ac58e66480d2351792bacef79f9d0ff24f34e8a9
Nouman Iftikhar <81663907+nouman-iftikhar@users.noreply.github.com>
2022-01-10 23:20:17 +0500

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,11 +1,11 @@
 resource "aws_ecs_cluster" "ecs" {
   name = var.name
 
-  capacity_providers = compact([
-    try(aws_ecs_capacity_provider.ecs_capacity_provider[0].name, ""),
-    "FARGATE",
-    "FARGATE_SPOT"
-  ])
+#   capacity_providers = compact([
+#     try(aws_ecs_capacity_provider.ecs_capacity_provider[0].name, ""),
+#     "FARGATE",
+#     "FARGATE_SPOT"
+#   ])
 
   lifecycle {
     ignore_changes = [
